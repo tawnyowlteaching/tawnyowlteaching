3908af7c2b3098505f7122acb31ff36371310994
jimbob3806 <jamesreid3806@gmail.com>
2019-06-13 19:52:13 +0100

    Update gatsby-config for PWA and correct metadata
    Include gatsby-plugin-offline as a plugin, and correct the
    position and details of the gatsby-plugin-manifest (manifest must
    be listed as a plugin before offline plugin).
    
    Update siteMetadata to reflect the current details of the project.

--- gatsby/tawnyowlteaching/gatsby-config.js
+++ gatsby/tawnyowlteaching/gatsby-config.js
@@ -1,34 +1,38 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: `TawnyOwlTeaching`,
+        description: `Teach your child the numeracy and 
+            literacy skills to get ahead at school.`,
+        author: `James Reid`
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `TawnyOwlTeaching`,
+                short_name: `TawnyOwl`,
+                start_url: `/`,
+                background_color: `#f7f0eb`,
+                theme_color: `#a2466c`,
+                display: `standalone`,
+                icon: `src/images/gatsby-icon.png`
+            }
+        },
+        `gatsby-plugin-offline`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            }
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`
+    ]
+    /*
+    Note gatsby-plugin-manifest must be listed BEFORE gatsby-plugin-offline 
+    when used in conjunction. 
+    See https://www.gatsbyjs.org/packages/gatsby-plugin-manifest/ for more info.
+    */
 }
